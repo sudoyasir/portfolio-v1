a0596186ba96f556ce216e4a1f93e7c6f2b60f2d
Yasir Nawaz <94890956+yasir2002@users.noreply.github.com>
2023-02-25 19:36:39 +0500

    loading screen added + delayed tawk.io widget for 10 seconfd

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
 import Main from './Main';
 import '../src/index.css'
+import '../src/App.css'
+import React, {useState, useEffect} from 'react';
+import HashLoader from "react-spinners/HashLoader";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+  setLoading(true)
+    setTimeout(()=>{
+      setLoading(false)
+    }, 5000)
+  }, [])
   return (
-    <Main />
+    <>
+    {
+      loading ?(
+        <div className='App'>
+          <HashLoader
+          color={'#18d26d'}
+          loading={loading}
+          size={100}
+          aria-label="Loading Spinner"
+          data-testid="loader"
+        />
+        </div>
+        )
+      
+        : <Main />
+
+    }
+    
+      
+    </>
   );
 }
 
