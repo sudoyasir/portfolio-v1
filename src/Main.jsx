7781c7b163a88c780c02105b2884d69e5b50805a
Yasir Nawaz <94890956+yasir2002@users.noreply.github.com>
2023-02-25 17:44:38 +0500

    responsive navigation bar

--- src/Main.jsx
+++ src/Main.jsx
@@ -9,16 +9,29 @@ export default function Main(){
     return(<>
     
     <section id='NavigationBar'>
-        <div className="container d-flex flex-row justify-content-between py-3 px-3">
-            <a href="https://yasir2002.github.io/portfolio/" className='logo'><img src={Logo} alt="" /> <span> &nbsp;Yasir</span></a>
-
-            <ul>
-                <li><a href="https://yasir2002.github.io/portfolio/" className='active'>Home</a></li>
-                <li><a href="#projects">Projects</a></li>
-                <li><a href="#footer">Contact</a></li>
-
-            </ul>
+    <div className="container">
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#"><img src={Logo} alt="" /> <b> &nbsp;Yasir</b></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                     <a class="nav-link active" aria-current="page" href="#NavigationBar">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#projects">Projects</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#footer">Contact</a>
+                    </li>
+                </ul>
+            </div>
         </div>
+    </nav>
+    </div>
     </section>
 
 
