1e978819233430714d59387aa0988a38dafbf85f
Yasir Nawaz <94890956+yasir2002@users.noreply.github.com>
2023-10-26 18:04:00 +0500

    started upgrade

--- src/App.js
+++ src/App.js
@@ -1,39 +1,10 @@
-import Main from './Main';
-import '../src/index.css'
-import '../src/App.css'
-import React, {useState, useEffect} from 'react';
-import HashLoader from "react-spinners/HashLoader";
+import './App.css';
 
 function App() {
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-  setLoading(true)
-    setTimeout(()=>{
-      setLoading(false)
-    }, 5000)
-  }, [])
   return (
-    <>
-    {
-      loading ?(
-        <div className='App'>
-          <HashLoader
-          color={'#18d26d'}
-          loading={loading}
-          size={100}
-          aria-label="Loading Spinner"
-          data-testid="loader"
-        />
-        </div>
-        )
-      
-        : <Main />
-
-    }
-    
-      
-    </>
+    <div>
+      Hello world!
+    </div>
   );
 }
 
