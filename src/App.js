2d72747b3392379b1b0db18495c4a4273d1a4150
Yasir Nawaz <94890956+yasir2002@users.noreply.github.com>
2023-10-29 18:47:10 +0500

    removed Main.jsx file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import "./App.css";
-import Main from "./Main";
-
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
+import ProjectCard from "./components/ProjectCard/ProjectCard";
+import Skills from "./components/Skills/Skills";
 
 function App() {
   return (
-    <Main />
+    <div>
+      <Header />
+      <Hero />
+      <Skills />
+      <ProjectCard />
+      <Footer />
+    </div>
   );
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,13 +5,13 @@ const Header = () => {
   return (
     <section id="NavigationBar" data-aos="fade" data-aos-duration="4000">
       <div className="container">
-        <nav class="navbar navbar-expand-lg navbar-light">
-          <div class="container-fluid">
-            <a class="navbar-brand" href="http://yasir2002.github.io/portfolio">
+        <nav className="navbar navbar-expand-lg navbar-light">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="http://yasir2002.github.io/portfolio">
               <img src={Logo} alt="" /> <b> &nbsp;Yasir</b>
             </a>
             <button
-              class="navbar-toggler"
+              className="navbar-toggler"
               type="button"
               data-bs-toggle="collapse"
               data-bs-target="#navbarSupportedContent"
@@ -19,26 +19,26 @@ const Header = () => {
               aria-expanded="false"
               aria-label="Toggle navigation"
             >
-              <span class="navbar-toggler-icon"></span>
+              <span className="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                <li class="nav-item">
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li className="nav-item">
                   <a
-                    class="nav-link active"
+                    className="nav-link active"
                     aria-current="page"
                     href="#NavigationBar"
                   >
                     Home
                   </a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#projects">
+                <li className="nav-item">
+                  <a className="nav-link" href="#projects">
                     Projects
                   </a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#footer">
+                <li className="nav-item">
+                  <a className="nav-link" href="#footer">
                     Contact
                   </a>
                 </li>
